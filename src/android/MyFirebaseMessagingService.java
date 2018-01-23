d91a0bcff55e414c9ff127bb95a31987cf12a276
leovilla89 <leonardo_villarroel@hotmail.com>
2018-01-22 23:37:37 -0300

    Update MyFirebaseMessagingService.java

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -98,6 +98,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setSmallIcon(getApplicationInfo().icon)
                 .setContentTitle(title)
                 .setContentText(messageBody)
+		.setStyle(new Notification.BigTextStyle().bigText(messageBody))
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
